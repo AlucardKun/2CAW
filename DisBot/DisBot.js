cac02ca5f62967ddc995d78759a216953bed2ddd
AlucarKun <rbse22@gmail.com>
2021-04-05 21:40:56 -0300

    Cadastro de turma

--- DisBot/DisBot.js
+++ DisBot/DisBot.js
@@ -1,3 +1,22 @@
-function alertaTeste() {
-    alert('Teste');
+function cadastrarTurma() {
+    var cbTurno = document.querySelector('input[name=turno]:checked');
+    var professor = document.getElementById('professor');
+    var disciplina = document.getElementById('disciplina');
+
+    if (!cbTurno) {
+        alert('Escolha um Turno!');
+        exit;
+    }
+
+    if (professor.value.trim() == "") {
+        alert('Entre com o nome do Professor!');
+        exit;
+    }
+
+    if (disciplina.value.trim() == "") {
+        alert('Entre com o nome da Turma!');
+        exit;
+    }
+
+    alert('Turma ' + disciplina.value.toUpperCase() + ' com o professor(a) ' + professor.value + ' do Turno ' + cbTurno.value + ' registrada.');
 }
