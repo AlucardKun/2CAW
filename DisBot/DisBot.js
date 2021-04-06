09a2267de297a018bf72b8c891cdef874bf5b4f1
AlucarKun <rbse22@gmail.com>
2021-04-05 22:00:23 -0300

    pegando texto do radio

--- DisBot/DisBot.js
+++ DisBot/DisBot.js
@@ -3,20 +3,31 @@ function cadastrarTurma() {
     var professor = document.getElementById('professor');
     var disciplina = document.getElementById('disciplina');
 
+    if (validaDados() == true) {
+        alert('Turma "' + disciplina.value.toUpperCase() + '" com o professor(a) "' + professor.value + '" do turno "' + cbTurno.attributes['aria-texto'].value + '" registrada com sucesso.');
+    }
+}
+
+function validaDados() {
+
+    var cbTurno = document.querySelector('input[name=turno]:checked');
+    var professor = document.getElementById('professor');
+    var disciplina = document.getElementById('disciplina');
+
     if (!cbTurno) {
         alert('Escolha um Turno!');
-        exit;
+        return false;
     }
 
     if (professor.value.trim() == "") {
         alert('Entre com o nome do Professor!');
-        exit;
+        return false;
     }
 
     if (disciplina.value.trim() == "") {
         alert('Entre com o nome da Turma!');
-        exit;
+        return false;
     }
 
-    alert('Turma ' + disciplina.value.toUpperCase() + ' com o professor(a) ' + professor.value + ' do Turno ' + cbTurno.value + ' registrada.');
+    return true;
 }
